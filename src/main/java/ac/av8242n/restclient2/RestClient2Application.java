a83ed0540f446ffdb9ef3981c54cf52cf7acd053
Archana Visveswaran <A.Visveswaran@greenwich.ac.uk>
2019-01-03 08:55:04 +0000

    Initial commit

---
+++ src/main/java/ac/av8242n/restclient2/RestClient2Application.java
@@ -0,0 +1,14 @@
+package ac.av8242n.restclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClient2Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/ac/av8242n/restclient2/RestClient2ApplicationTests.java
@@ -0,0 +1,17 @@
+package ac.av8242n.restclient2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestClient2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
